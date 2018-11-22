95f67a6f703fe4dbed85253e3ca9cb189ddaca87
HanjuTsai <rita@dentall.io>
2018-11-22 22:26:34 +0800

    finish database

---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from datetime import datetime, timedelta
+
+
+# Create your models here.
+class User(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE , related_name = "account")
+    uname = models.CharField(max_length = 20 , blank = False)
+    nickname = models.CharField(max_length = 20)
+    ntumail = models.CharField(max_length = 20 , blank = False)
+    profile = models.ImageField(upload_to = 'profiles', blank=True)
+
+class Wallet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = "user" )
+    amount = models.IntegerField(blank = False)
+    date = models.DateTimeField(auto_now_add = True)
+
+
+class Product(models.Model):
+    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name = "seller")
+    price = models.IntegerField(blank=False)
+    amount = models.IntegerField(default = 1)
+    released_date = models.DateTimeField(auto_now_add = True)
+    expired_date = models.DateTimeField(default = datetime.now() + timedelta(days = 30))
+    profile = models.ImageField(upload_to = 'products', blank = False)
+    information = models.CharField(max_length = 200, blank = True)
+
+
+class Message(models.Model):
+    sent_frome = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete = models.CASCADE, related_name = "sent_from")
+    sent_to = models.ForeignKey(User, on_delete = models.CASCADE , related_name = "sent_to")
+    msg = models.CharField(max_length = 100, blank = False)
+    date = models.DateTimeField(auto_now_add = True)

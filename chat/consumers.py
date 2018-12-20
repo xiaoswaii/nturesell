95a65b5b776af72ae22649ca3846f1ad1700e58a
HanjuTsai <rita@dentall.io>
2018-12-21 00:52:40 +0800

    update

--- chat/consumers.py
+++ chat/consumers.py
@@ -12,7 +12,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
